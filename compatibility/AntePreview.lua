338a11f90b45dd5af00cd3730b2f3ec4669f4af8
DigitalDetective47 <ninji2701@gmail.com>
2025-03-21 04:44:07 -0400

    update Next Ante Preview compatibility for ver. 0.2

--- compatibility/AntePreview.lua
+++ compatibility/AntePreview.lua
@@ -3,13 +3,9 @@ if next(SMODS.find_mod("AntePreview")) then
     local predict_next_ante_ref = predict_next_ante
     function predict_next_ante()
         local predictions = predict_next_ante_ref()
-        if G.LOBBY.code then
-            if G.LOBBY.config.gamemode == "attrition" and G.GAME.round_resets.ante > 1 then
-                predictions.Boss.blind = "bl_pvp"
-            elseif G.LOBBY.config.gamemode == "showdown" and G.GAME.round_resets.ante >= G.LOBBY.config.showdown_starting_antes then
-                predictions.Small.blind = "bl_pvp"
-                predictions.Big.blind = "bl_pvp"
-                predictions.Boss.blind = "bl_pvp"
+        if MP.LOBBY.code then
+            if G.GAME.round_resets.ante > 1 then
+                predictions.Boss.blind = "bl_mp_nemesis"
             end
         end
         return predictions
