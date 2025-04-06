8ebffb39c63fcdbf9e98efabff6d9f8994801e52
Frying☆Pan <fryingpan@team-prism.rocks>
2025-04-05 22:21:01 -0400

    Fix spelling of G.C.MULTIPLAYER (it was MULITPLAYER)

--- objects/stickers/balanced.lua
+++ objects/stickers/balanced.lua
@@ -8,7 +8,7 @@ SMODS.Atlas({
 SMODS.Sticker({
 	key = "sticker_balanced",
 	atlas = "sticker_balanced",
-	badge_colour = G.C.MULITPLAYER,
+	badge_colour = G.C.MULTIPLAYER,
 	default_compat = false,
 	needs_enable_flag = true,
 	hide_badge = true,
