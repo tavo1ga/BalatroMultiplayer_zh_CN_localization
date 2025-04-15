aed88a0879c2b9dbc44e59625377b5e8294fc484
toneblock <55140928+Toneblock@users.noreply.github.com>
2025-04-15 15:53:44 +0400

    fix glass

--- rulesets/standard.lua
+++ rulesets/standard.lua
@@ -54,6 +54,9 @@ SMODS.Joker({
 
 SMODS.Enhancement:take_ownership("glass", {
 	set_ability = function(self, card, initial, delay_sprites)
-		card.ability.Xmult = MP.LOBBY.config.ruleset == "ruleset_mp_standard" and MP.LOBBY.code and 1.5 or 2
+		local x = MP.LOBBY.config.ruleset == "ruleset_mp_standard" and MP.LOBBY.code and 1.5 or 2
+		-- Xmult is display, x_mult is internal. don't ask why, i don't know
+		card.ability.Xmult = x
+		card.ability.x_mult = x
 	end,
 }, true)
