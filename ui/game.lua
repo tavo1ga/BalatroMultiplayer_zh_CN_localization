e0e5a6c67dd3a3b149add221268b21274c249b7a
dunghmm <142342408+dunghmm@users.noreply.github.com>
2025-04-30 20:37:38 +0700

    Fix end round events triggering twice under specific circumstances

--- ui/game.lua
+++ ui/game.lua
@@ -927,6 +927,9 @@ function Game:update_new_round(dt)
 end
 
 function MP.end_round()
+	if MP.GAME.antes_keyed[MP.GAME.ante_key] then
+		return
+	end
 	G.GAME.blind.in_blind = false
 	local game_over = false
 	local game_won = false
