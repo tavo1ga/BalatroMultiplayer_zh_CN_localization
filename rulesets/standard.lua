046172ab3daafd9ee513f6989c635fded07d1f93
toneblock <55140928+Toneblock@users.noreply.github.com>
2025-07-14 21:04:24 +0100

    remove original

--- rulesets/standard.lua
+++ rulesets/standard.lua
@@ -36,46 +36,6 @@ MP.Ruleset({
 	},
 }):inject()
 
-SMODS.Joker({
-	key = "hanging_chad",
-	no_collection = true,
-	unlocked = true,
-	discovered = true,
-	blueprint_compat = true,
-	perishable_compat = true,
-	eternal_compat = true,
-	rarity = 1,
-	cost = 4,
-	pos = { x = 9, y = 6 },
-	config = { extra = 1, mp_sticker_balanced = true },
-	loc_vars = function(self, info_queue, card)
-		return { vars = {
-			card.ability.extra,
-		} }
-	end,
-	calculate = function(self, card, context)
-		if context.cardarea == G.play and context.repetition then
-			if context.other_card == context.scoring_hand[1] then
-				return {
-					message = localize("k_again_ex"),
-					repetitions = card.ability.extra,
-					card = card,
-				}
-			end
-			if context.other_card == context.scoring_hand[2] then
-				return {
-					message = localize("k_again_ex"),
-					repetitions = card.ability.extra,
-					card = card,
-				}
-			end
-		end
-	end,
-	in_pool = function(self)
-		return MP.LOBBY.config.ruleset == "ruleset_mp_standard" and MP.LOBBY.code
-	end,
-})
-
 SMODS.Joker:take_ownership("hanging_chad", {
 	loc_vars = function(self, info_queue, card) -- This is dumb but there's no original loc_vars to override, if i knew how to fix that i would
 		return { vars = { card.ability.extra } }
@@ -87,14 +47,14 @@ SMODS.Joker:take_ownership("hanging_chad", {
 MP.ReworkCenter({
 	key = "j_hanging_chad", -- Required, format is how it appears in G.P_CENTERS (prefixes are required)
 	ruleset = "standard", -- Required, format is the same as the ruleset key
-	config = { extra = 1 },	-- We can add whatever value in the center we want to override, here we're changing the config
+	config = { extra = 1 }, -- We can add whatever value in the center we want to override, here we're changing the config
 	loc_vars = function(self, info_queue, card) -- We will need to return a modified localization key here due to the effect change
 		return {
 			key = self.key.."_mp_standard",
 			vars = { card.ability.extra } 
 		}
 	end,
-	calculate = function(self, card, context)	-- This overrides the calculate, same format as SMODS.Joker
+	calculate = function(self, card, context) -- This overrides the calculate, same format as SMODS.Joker
 		if context.cardarea == G.play and context.repetition then
 			if context.other_card == context.scoring_hand[1] 
 			or context.other_card == context.scoring_hand[2] then
