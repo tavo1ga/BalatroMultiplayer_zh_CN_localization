9a40a52bb310d65a87b43486cc36b83920d0b44c
toneblock <55140928+Toneblock@users.noreply.github.com>
2025-07-22 23:16:10 +0100

    quick double end fix

--- ui/game.lua
+++ ui/game.lua
@@ -1067,6 +1067,9 @@ function Game:update_new_round(dt)
 end
 
 function MP.end_round()
+	if MP.stop_ends then sendDebugMessage('Double-end prevented'); return end -- quick fix to prevent double ends?
+	MP.stop_ends = true
+	
 	G.GAME.blind.in_blind = false
 	local game_over = false
 	local game_won = false
@@ -2365,6 +2368,7 @@ function G.FUNCS.select_blind(e)
 	MP.GAME.prevent_eval = false
 	select_blind_ref(e)
 	if MP.LOBBY.code then
+		MP.stop_ends = false -- wrapping blind so ends don't happen twice, referenced at start of MP.end_round
 		MP.GAME.ante_key = tostring(math.random())
 		MP.ACTIONS.play_hand(0, G.GAME.round_resets.hands)
 		MP.ACTIONS.new_round()
