dfe3c3354cd1cc6f85432d49cbb229bf4b278c0b
Connor Mills <connor@virtualized.dev>
2025-08-04 03:00:09 -0700

    Removed weekly options for now

--- ui/main_menu.lua
+++ ui/main_menu.lua
@@ -216,7 +216,6 @@ function G.UIDEF.ruleset_selection_options()
 			buttons = {
 				{ button_id = "badlatro_ruleset_button", button_localize_key = "k_badlatro" },
 				{ button_id = "sandbox_ruleset_button", button_localize_key = "k_sandbox" },
-				{ button_id = "weekly_ruleset_button",   button_localize_key = "k_weekly" },
 			}
 		}
 	}
